cec76bbe96ed1ed732a0c82237242c9074323dbf
chrissschin <solischris92@gmail.com>
2016-01-10 07:56:39 -0800

    got the days down

--- app/app.js
+++ app/app.js
@@ -1,3 +1,11 @@
-angular.module('calendarDemoApp', []);
+angular.module('calendarDemoApp', [])
+  .controller('mainCtrl', function($scope) {
+    console.log(CalendarRange.getMonthlyRange(new Date()));
+    console.log(CalendarRange.prepareDate(new Date()));
+
+    $scope.date = CalendarRange.getMonthlyRange(new Date());
+
+
+  });
 
 // your controller and directive code go here
