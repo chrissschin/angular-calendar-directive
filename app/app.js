3d1b5d66fac49aa8903cf5895bd7deadddd07b50
chrissschin <solischris92@gmail.com>
2016-01-13 17:37:53 -0800

    finished

--- app/app.js
+++ app/app.js
@@ -3,9 +3,51 @@ angular.module('calendarDemoApp', [])
     console.log(CalendarRange.getMonthlyRange(new Date()));
     console.log(CalendarRange.prepareDate(new Date()));
 
-    $scope.date = CalendarRange.getMonthlyRange(new Date());
+
+  })
+  .directive('myCalendar', function() {
+    return {
+      restrict : 'E',
+      templateUrl : 'mycalendar.html',
+      scope : false,
+      controller : function($scope, $element, $attrs) {
+
+        $scope.date = CalendarRange.getMonthlyRange(new Date());
+
+        $scope.months = ["January","Febuary","March","April","May","June","July","August","September","October","November","December"];
+        $scope.currentDate = CalendarRange.prepareDate(new Date());
+
+        $scope.monthSelected = $scope.months[$scope.currentDate.month];
+        $scope.yearSelected = $scope.currentDate.year;
+        $scope.currentMonth = $scope.months.indexOf($scope.monthSelected);
+
+        $scope.years = [];
+
+        for (var i = ($scope.yearSelected - 20); i <= ($scope.yearSelected + 20); i++) {
+          $scope.years.push(i);
+        };
+
+        $scope.dateChange = function(month, year){
+          var dateSelected = new Date(month + " " + year);
+          console.log(dateSelected);
+
+          $scope.currentMonth = $scope.months.indexOf(month);
+
+          $scope.date = CalendarRange.getMonthlyRange(dateSelected);
+          console.log($scope.date);
 
 
+
+
+
+        };
+
+
+      },
+      link : function(scope, element, attrs) {
+
+      }
+    }
   });
 
 // your controller and directive code go here
